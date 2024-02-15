4a9c9da9a9496730252880fb861696c79a649774
Nguyễn Dương Quốc Thái <quocthai0099@gmail.com>
2024-02-15 21:42:02 +0700

    I just delete all

--- task-17-sub-01.js
+++ task-17-sub-01.js
@@ -1,6 +1,4 @@
 /**
  * Completed task 17 sub 01
  */
-console.log("task 17 sub 1");
-console.log("Test task 17 sub 1")
-console.log("Test task 17 sub 2")
+console.log("I just delete all");
--- task-18-sub-01.js
+++ task-18-sub-01.js
@@ -2,6 +2,5 @@
  * Completed task 18 sub 01
  */
 
-console.log("This is task 18")
+console.log("This is task 18");
 console.log("Updat this task 18 more");
-
